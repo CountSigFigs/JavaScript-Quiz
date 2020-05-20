d2c311ea41499e2d17526d122be1d1157f1cd8c4
Brian Holt <bharrisonholt@gmail.com>
2020-05-20 14:40:44 -0700

    finshing landing

--- src/App.js
+++ src/App.js
@@ -8,56 +8,73 @@ import { Button } from '@material-ui/core';
 
 const jsLogo = require('./Images/JavaScript.png');
 
-const styles={
-  paper:{
-    textAlign:'center',
-    padding:10,
-    margin:10
+const styles = {
+  container: {
+    backgroundColor: 'black',
+    height: 575,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  title:{
-    fontWeight:'bold',
-    fontSize:32,
-    marginTop:7,
-    marginBottom:7
+  bigPaper: {
+    padding: 30,
   },
-  button:{
-    margin:4
+  paper: {
+    textAlign: 'center',
+    padding: 10,
+    margin: 10,
+    height:324,
+    display:'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems:'center'
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 32,
+    marginTop: 7,
+    marginBottom: 7
+  },
+  button: {
+    margin: 4
   }
 }
 
 class App extends Component {
 
 
-  render(){
-  return (
-    <Box className="App">
-      <Container maxWidth='md'>
-       <Grid
-        container
-        direction='row'
-        justify='center'
-        alignItems='center'
-      >
-        <Grid item sm={7}>
-            <Paper style={styles.paper}>
-              <img src={jsLogo} alt='javscript logo'></img>
-            </Paper>
-        </Grid>
-        <Grid item sm={5}>
-          <Paper style={styles.paper}>
-            <div style={styles.title}>JavaScript Knowledge Quiz</div>
-            <div>Test your knowledge of JavaScript by answering this 10 question quiz.Click the button 
+  render() {
+    return (
+      <Box className="App">
+        <Container maxWidth='md' style={styles.container}>
+          <Paper style={styles.bigPaper}>
+            <Grid
+              container
+              direction='row'
+              justify='center'
+              alignItems='center'
+            >
+              <Grid item sm={7}>
+                <Paper style={styles.paper}>
+                  <img src={jsLogo} alt='javscript logo'></img>
+                </Paper>
+              </Grid>
+              <Grid item sm={5}>
+                <Paper style={styles.paper}>
+                  <div style={styles.title}>JavaScript Quiz</div>
+                  <div>Test your knowledge of JavaScript by answering this 10 question quiz.Click the button
             below when you are ready to begin.</div>
-            <Button variant="contained" color="primary" style={styles.button}>
-              Start
+                  <Button variant="contained" color="primary" style={styles.button}>
+                    Start
             </Button>
+                </Paper>
+              </Grid>
+            </Grid>
           </Paper>
-        </Grid>
-       </Grid>
-      </Container>
-    </Box>
-  );
-}
+        </Container>
+      </Box>
+    );
+  }
 }
 
 export default App;
