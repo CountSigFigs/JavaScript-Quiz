6dc3ca9b1518a18b4295676c457a48b761ada8aa
Brian Holt <bharrisonholt@gmail.com>
2020-05-21 11:31:46 -0700

    router setup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 import Box from '@material-ui/core/Box';
 import { Container } from '@material-ui/core';
-import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import { Button } from '@material-ui/core';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import StartingComponent from './Components/StartingComponent'
+import Game from './Components/GameComponent';
 
-const jsLogo = require('./Images/JavaScript.png');
+class App extends Component {
+
+
+  render() {
+    return (
+      <Router>
+        <Box className="App">
+          <Container maxWidth='md' style={styles.container}>
+            <Paper style={styles.bigPaper} elevation={2}>
+            <Switch>
+              <Route exact path="/">             
+                <StartingComponent/>
+              </Route>
+              <Route path="/game">             
+                <Game/>
+              </Route>
+            </Switch>
+            </Paper>
+          </Container>
+        </Box>
+      </Router>
+    );
+  }
+}
 
 const styles = {
   container: {
@@ -17,63 +41,7 @@ const styles = {
     alignItems: 'center'
   },
   bigPaper: {
-    padding: 30,
-  },
-  paper: {
-    textAlign: 'center',
-    padding: 10,
-    margin: 10,
-    height:324,
-    display:'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems:'center'
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 32,
-    marginTop: 7,
-    marginBottom: 7
-  },
-  button: {
-    margin: 4
-  }
-}
-
-class App extends Component {
-
-
-  render() {
-    return (
-      <Box className="App">
-        <Container maxWidth='md' style={styles.container}>
-          <Paper style={styles.bigPaper}>
-            <Grid
-              container
-              direction='row'
-              justify='center'
-              alignItems='center'
-            >
-              <Grid item sm={7}>
-                <Paper style={styles.paper}>
-                  <img src={jsLogo} alt='javscript logo'></img>
-                </Paper>
-              </Grid>
-              <Grid item sm={5}>
-                <Paper style={styles.paper}>
-                  <div style={styles.title}>JavaScript Quiz</div>
-                  <div>Test your knowledge of JavaScript by answering this 10 question quiz.Click the button
-            below when you are ready to begin.</div>
-                  <Button variant="contained" color="primary" style={styles.button}>
-                    Start
-            </Button>
-                </Paper>
-              </Grid>
-            </Grid>
-          </Paper>
-        </Container>
-      </Box>
-    );
+    padding: 75,
   }
 }
 
---
+++ src/Components/StartingComponent.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react'
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { Button } from '@material-ui/core';
+import {Link} from 'react-router-dom'
+const jsLogo = require('../Images/JavaScript.png');
+
+
+class StartingComponent extends Component{
+
+    render (){
+        return(
+            <Grid
+            container
+            direction='row'
+            justify='center'
+            alignItems='center'
+          >
+            <Grid item sm={7}>
+              <Paper style={styles.paper} elevation={3}>
+                <img src={jsLogo} alt='javscript logo'></img>
+              </Paper>
+            </Grid>
+            <Grid item sm={5}>
+              <Paper style={styles.paper} elevation={3}>
+                <div style={styles.title}>JavaScript Quiz</div>
+                <div>Test your knowledge of JavaScript by answering this 10 question quiz.Click the button
+          below when you are ready to begin.</div>
+                <Link to="/game">
+                    <Button variant="contained" color="primary" style={styles.button}> Start</Button>
+                </Link>
+              </Paper>
+            </Grid>
+          </Grid>
+        )
+    }
+}
+
+const styles = {
+    paper: {
+      textAlign: 'center',
+      padding: 10,
+      margin: 10,
+      height: 324,
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    title: {
+      fontWeight: 'bold',
+      fontSize: 32,
+      marginTop: 7,
+      marginBottom: 7
+    },
+    button: {
+      margin: 4
+    }
+  }
+
+export default StartingComponent;
