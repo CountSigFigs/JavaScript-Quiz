6db45615a063f856e12d0015f69d5542f1419510
Brian Holt <bharrisonholt@gmail.com>
2020-05-31 16:36:34 -0700

    Updates

---
+++ precache-manifest.8a315ee651e3bed84602c27cef8c3752.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d04a3e3af186a9176798d3676c82f7",
+    "url": "/JavaScript-Quiz/index.html"
+  },
+  {
+    "revision": "e2a8002d8f3076fac7ff",
+    "url": "/JavaScript-Quiz/static/css/main.eca59ae4.chunk.css"
+  },
+  {
+    "revision": "35b9841e5c323551b496",
+    "url": "/JavaScript-Quiz/static/js/2.03153d37.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/JavaScript-Quiz/static/js/2.03153d37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2a8002d8f3076fac7ff",
+    "url": "/JavaScript-Quiz/static/js/main.c0ada462.chunk.js"
+  },
+  {
+    "revision": "f0e6e961e3845b8fb650",
+    "url": "/JavaScript-Quiz/static/js/runtime-main.13ad5036.js"
+  },
+  {
+    "revision": "5839fcae9c99eb257e32ff058267aba6",
+    "url": "/JavaScript-Quiz/static/media/JavaScript.5839fcae.png"
+  },
+  {
+    "revision": "a4e23166f466b5eee21131f2239a4a29",
+    "url": "/JavaScript-Quiz/static/media/jsjoke.a4e23166.png"
+  }
+]);
